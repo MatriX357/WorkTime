4cc93df6ca0c4a198a313b8edf544a68d1288038
Maciek <maciej.nalewajka@smcebi.edu.pl>
2018-04-26 19:44:56 +0200

    Znowu update layoutów rejestracja i main i projekt

--- app/src/main/java/pl/maciejnalewajka/worktime/Main.java
+++ app/src/main/java/pl/maciejnalewajka/worktime/Main.java
@@ -9,7 +9,7 @@ public class Main extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_nowy_projekt);
     }
 
     public void rejestracja(View view) {
