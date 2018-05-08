f3177b4e087302cb485ee81a9554f2e50cdc8158
Maciek <maciej.nalewajka@smcebi.edu.pl>
2018-05-08 19:01:46 +0200

    Sprawdzanie pól w Main.

--- app/src/main/java/pl/maciejnalewajka/worktime/Main.java
+++ app/src/main/java/pl/maciejnalewajka/worktime/Main.java
@@ -3,13 +3,19 @@ package pl.maciejnalewajka.worktime;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Main extends AppCompatActivity {
+    EditText text_login;
+    EditText text_haslo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        text_login = findViewById(R.id.editText6);
+        text_haslo = findViewById(R.id.editText5);
     }
 
     public void rejestracja(View view) {
@@ -17,7 +23,35 @@ public class Main extends AppCompatActivity {
     }
 
     public void zaloguj(View view) {
-        setContentView(R.layout.activity_master);
+
+
+        // Sprawdzenie pól
+        //login
+        boolean l = false;
+        int cyfry = 0;
+        char[] znaki = text_login.getText().toString().toCharArray();
+        for(int i=0; i<znaki.length; i++){
+            char znak = znaki[i];
+            if(znak == '@'){l = true;}
+        }
+        //hasło
+        char[] znaki2 = text_haslo.getText().toString().toCharArray();
+        for(int i=0; i<znaki2.length; i++){
+            char znak = znaki[i];
+            if(znak>='0' && znak<='9')
+            {
+                cyfry ++;
+            }
+        }
+        //Sprawdzenie
+        if(l == false){
+            Toast.makeText(this, "Podaj poprawny e-mail!", Toast.LENGTH_SHORT).show();
+        }
+        else{
+            if(text_haslo.getText().toString().length() < 8 && cyfry < 2){
+                Toast.makeText(this, "Podaj poprawne hasło!", Toast.LENGTH_SHORT).show();
+            }
+            else{setContentView(R.layout.activity_master);}}
     }
 
     public void anuluj(View view) {
